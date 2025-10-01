50ece78bc21a3720eaccdc914bbac8376bf00906
gyoreviktor <gyoreviktor03@gmail.com>
2025-10-01 20:52:27 +0200

    main

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,51 +1,51 @@
-#include <chrono>
-#include <functional>
-#include <memory>
-#include <string>
-#include <cmath> 
-
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float32.hpp"
+#include <cmath>
+#include <chrono>
 
-using namespace std::chronoliterals;
+using namespace std::chrono_literals;
 
-class COTG : public rclcpp::Node
+class Generator : public rclcpp::Node
 {
 public:
-    COTG() : Node("jelgen"), count(0)
-        {
-            RCLCPP_INFO(this->getlogger(), "Szinusz jelek előállítása");
-            pub1 = this->create_publisher<stdmsgs::msg::Float32>("cotg1", 10);
-            pub2 = this->create_publisher<stdmsgs::msg::Float32>("cotg2", 10);
-            timer = this->create_wall_timer(50ms, std::bind(&COTG::timer_callback, this));
-        }
+    Generator() : Node("generator"), count(0)
+    {
+        pub1 = this->create_publisher<std_msgs::msg::Float32>("cotg1", 10);
+        pub2 = this->create_publisher<std_msgs::msg::Float32>("cotg2", 10);
 
+        timer = this->create_wall_timer(
+            50ms, std::bind(&Generator::timer_callback, this));
+    }
 
 private:
     void timer_callback()
     {
         auto msg1 = std_msgs::msg::Float32();
-        auto msg2 = stdmsgs::msg::Float32();
+        auto msg2 = std_msgs::msg::Float32();
 
-        auto t = count * 0.01;
-        msg1.data = 1.0 / tan(t * 2M_PI1*1) * 2;;
-        msg1.data = 1.0 / tan(t * 2M_PI2*2) * 0.5;
+        double t = count * 0.01;
+
+        msg1.data = 1.0 / tan(t * 2 * M_PI * 1) * 2.0;
+        msg2.data = 1.0 / tan(t * 2 * M_PI * 2) * 0.5;
 
         pub1->publish(msg1);
         pub2->publish(msg2);
 
+        RCLCPP_INFO(this->get_logger(), "Publishing: cotg1=%.3f cotg2=%.3f", msg1.data, msg2.data);
+
         count++;
     }
 
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub1;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub2;
     rclcpp::TimerBase::SharedPtr timer;
-    rclcpp::Publisher<stdmsgs::msg::Float32>::SharedPtr pub1, pub2_;
-    sizet count;
+    size_t count;
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<COTG>());
+    rclcpp::spin(std::make_shared<Generator>());
     rclcpp::shutdown();
     return 0;
 }

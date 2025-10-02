b03c9bff7938563fa3f3f31a6c0a86c476c6ef1b
gyoreviktor <gyoreviktor03@gmail.com>
2025-10-02 10:17:44 +0200

    saddd

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,51 +1,42 @@
 #include <chrono>
-#include <functional>
 #include <memory>
-#include <string>
-#include <cmath> 
-
+#include <random>
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/float32.hpp"
+#include "std_msgs/msg/int32.hpp"
 
 using namespace std::chrono_literals;
 
-class koszinusz : public rclcpp::Node
+class NumberGenerator : public rclcpp::Node
 {
 public:
-    koszinusz() : Node("generator"), count_(0)
-        {
-            RCLCPP_INFO(this->get_logger(), "koszinusz jelek előállítása");
-            pub1_ = this->create_publisher<std_msgs::msg::Float32>("cos1", 10);
-            pub2_ = this->create_publisher<std_msgs::msg::Float32>("cos2", 10);
-            timer_ = this->create_wall_timer(50ms, std::bind(&koszinusz::timer_callback, this));
-        }
-    
-
-private:
-    void timer_callback()
+    NumberGenerator()
+    : Node("number_generator"), mt(rd()), dist(0, 100)
     {
-        auto msg1 = std_msgs::msg::Float32();
-        auto msg2 = std_msgs::msg::Float32();
-
-        auto t = count_ * 0.01;
-        msg1.data = cos(t * 7*M_PI*2) * 5;
-        msg2.data = cos(t * 3*M_PI*6) * 2;
-
-        pub1_->publish(msg1);
-        pub2_->publish(msg2);
-
-        count_++;
+        publisher_ = this->create_publisher<std_msgs::msg::Int32>("numbers", 10);
+        timer_ = this->create_wall_timer(
+            100ms, std::bind(&NumberGenerator::publish_number, this));
     }
 
+private:
+    void publish_number()
+    {
+        auto message = std_msgs::msg::Int32();
+        message.data = dist(mt);  // 0–100 véletlen szám
+        publisher_->publish(message);
+        RCLCPP_INFO(this->get_logger(), "Generated: %d", message.data);
+    }
+
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub1_, pub2_;
-    size_t count_;
+    std::random_device rd;
+    std::mt19937 mt;
+    std::uniform_int_distribution<int> dist;
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<koszinusz>());
+    rclcpp::spin(std::make_shared<NumberGenerator>());
     rclcpp::shutdown();
     return 0;
 }

9cbe9639993dd16dad3a0b13603640010fa2ddb7
KPredi <gyoreviktor03@gmail.com>
2025-10-06 08:16:13 +0200

    llk

--- launch/indito.py
+++ launch/indito.py
@@ -3,34 +3,9 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(
-            package='gyo_xj0_autonom',
-            executable='generator',
-            name='generator',
-            output='screen'
-        ),
-        Node(
-            package='gyo_xj0_autonom',
-            executable='generator_minusz',
-            name='generator_minusz',
-            output='screen'
-        ),
-        Node(
-            package='gyo_xj0_autonom',
-            executable='atlagolo',
-            name='atlagolo',
-            output='screen'
-        ),
-        Node(
-            package='gyo_xj0_autonom',
-            executable='atlagolo_minusz',
-            name='atlagolo_minusz',
-            output='screen'
-        ),
-        Node(
-            package='gyo_xj0_autonom',
-            executable='atlagolo_ossz',
-            name='atlagolo_ossz',
-            output='screen'
-        ),
+        Node(package='gyo_xj0_autonom', executable='atlagolo', name='atlagolo', output='screen'),
+        Node(package='gyo_xj0_autonom', executable='generator',    name='generator',    output='screen'),
+        Node(package='gyo_xj0_autonom', executable='atlagolo_ossz',  name='atlagolo_ossz',  output='screen'),
+        Node(package='gyo_xj0_autonom', executable='atlagolo_minusz', name='atlagolo_minusz', output='screen'),
+        Node(package='gyo_xj0_autonom', executable='generator_minusz',    name='generator_minusz',    output='screen'),
     ])
